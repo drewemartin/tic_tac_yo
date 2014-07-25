18246d4e1e1d8ea8efaddb7c2594e222ff2e011c
Sandy Ludosky <sandyludosky@gmail.com>
2014-07-24 20:30:15 -0400

    ui users#login users#sign_up

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,4 @@ module TicTacYo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
-  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 end
