6478cb86e9ea4443313f3cca03d6b8bc52b9ca20
zhengweiyi <zhengweiyi@gmail.com>
2014-07-26 15:55:40 -0400

    implement user edit profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,19 @@ class UsersController < ApplicationController
 
   end
 
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     @favorite = Favorite.where("favoriter_id = ? AND favorited_id = ?",current_user.id, @user.id).first
