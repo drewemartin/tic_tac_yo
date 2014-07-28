be24590bff08a1aeba125e6406404b33777d350b
Sandy Ludosky <sandyludosky@gmail.com>
2014-07-28 15:53:54 -0400

    customization of private messages

--- app/assets/javascripts/private_messages.js
+++ app/assets/javascripts/private_messages.js
@@ -2,6 +2,8 @@ $(document).on('ready page:load', function(){
   $(".delete-btn").click(function(event){
 			event.preventDefault();
 			event.stopPropagation();
+			var yes = confirm("Are you sure?")
+			if (yes === true){
 			_this = $(this)
 			$.ajax({
 				url: $(this).attr('href'),
@@ -10,5 +12,6 @@ $(document).on('ready page:load', function(){
 			}).done(function(data){
 				_this.closest(".message-tr").remove();
 			});
+		}
   });
 });
