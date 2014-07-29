74f3ef1200f571c55ce2415272d8fa1f0dd7248d
zhengweiyi <zhengweiyi@gmail.com>
2014-07-28 22:29:42 -0400

    implement toggling of 'add to favorite' and 'remove from favorite' buttons using ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,20 +5,22 @@ class FavoritesController < ApplicationController
   	@favorite = current_user.favorites.build(favorited: user_to_add)
   	respond_to do |format|
   		if @favorite.save
-  			favorited_user = @favorite.favorited
-        #if the favorited_user is on the block list, remove it from the block list
-        Block.where(:blocker => current_user, :blocked => favorited_user).destroy_all
-  			format.json{render json: {first_name: favorited_user.first_name, last_name: favorited_user.last_name}}
+  			@user = @favorite.favorited
+        #if the user is on the block list, remove it from the block list
+        Block.where(:blocker => current_user, :blocked => @user).destroy_all
+        format.js { render partial: "favorites/destroy_favorite" }
   		end
   	end
   end
 
   def destroy
     favorite_to_delete = Favorite.find(params[:id])
-    favorited_user = favorite_to_delete.favorited
+    @user = favorite_to_delete.favorited
     respond_to do |format|
       if favorite_to_delete.destroy
-        format.json{render json:{first_name: favorited_user.first_name, last_name: favorited_user.last_name}}
+        format.json{render json:{first_name: @user.first_name, last_name: @user.last_name}}
+        format.js { render partial: "favorites/favorite" }
+
       end
     end
   end
