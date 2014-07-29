f75fb850b9506b4891f86eefa7b2093faf51a1d5
zhengweiyi <zhengweiyi@gmail.com>
2014-07-29 13:58:48 -0400

    modify seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,26 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.delete_all
 
-
-20.times do 
+40.times do 
+    first_name = Faker::Name.first_name
+    last_name = Faker::Name.last_name
+    email = (first_name + "_" + last_name).downcase + "@tictacyo.com"
+    username = first_name + " " + last_name
+    address = ["New York, NY, USA", "Toronto, ON, CANADA", "London, UK"].sample
   User.create!(
-    :email => Faker::Internet.email,
+    :email => email,
     :crypted_password => "12345",
+    :first_name => first_name,
+    :last_name => last_name,
+    :username => username,
     :salt => "sodium",
-    :first_name => Faker::Name.first_name,
-    :last_name => Faker::Name.last_name,
-    :username => Faker::Internet.user_name,
     :gender => ["male", "female"].sample,
     :gender_preference => ["any","female", "male"].sample,
-    :address => "#{(1..700).to_a.sample} W 45th St New York, NY 10036, USA",
-    :biography => Faker::Lorem.sentence(2),
-    :date_of_birth => Time.now - ((18..45).to_a.sample).years
+    :address => address,
+    :biography => Faker::Lorem.paragraph(2),
+    :date_of_birth => Time.now - ((18..70).to_a.sample).years
     )
+    sleep 0.5
 end
 
  
