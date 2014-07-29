40a079e9790d963062a3c4e3780c3533d36f3753
Sandy Ludosky <sandyludosky@gmail.com>
2014-07-29 18:33:45 -0400

    removed fonts

--- app/assets/javascripts/animate.js
+++ app/assets/javascripts/animate.js
@@ -1,7 +1,7 @@
 // JavaScript Document
 
 $(document).ready(function() {
-      $('#navigation-menu a[href^="#"]').click(function() {
+      $('#navigation-menu a[href^="/#"]').click(function() {
           var target = $(this.hash);
           if (target.length == 0) target = $('#navigation-menu a[name="' + this.hash.substr(1) + '"]');
           if (target.length == 0) target = $('html');
