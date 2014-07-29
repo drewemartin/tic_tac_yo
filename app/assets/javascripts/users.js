be98c288a537c85dabdc294ec539905179828c2f
zhengweiyi <zhengweiyi@gmail.com>
2014-07-29 18:09:11 -0400

    fix search function

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,34 +1,43 @@
 
+// # Place all the behaviors and hooks related to the matching controller here.
+// # All this logic will automatically be available in application.js.
+// # You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).on('ready page:load', function(){
 
-$(document).ready(function() {
-	var height = $(window).height();	
-    $('.main').css('height', height);
-    $('.bottom').css('height', height);
+  var height = $(window).height();  
+      $('.main').css('height', height);
+      $('.bottom').css('height', height);
 
-    
-$('.filtering').slick({
-   slidesToShow: 4,
-    slidesToScroll: 1,
-    arrows: true,
-    autoplay: true,
-    autoplaySpeed: 7000, 
-    cssEase: 'ease',
-    dots: 'true'
-  });
+  window.initializeSlider = function() {
+    console.log("Initializing slider!");
+    $('.filtering').unslick();
+    $('.filtering').slick({
+     slidesToShow: 4,
+      slidesToScroll: 1,
+      arrows: true,
+      autoplay: true,
+      autoplaySpeed: 7000, 
+      cssEase: 'ease',
+      dots: 'true'
+    });
+  }
+
+  initializeSlider(); 
+
 
 var filtered = false;
 
-$('.js-filter').on('click', function(){
-  if(filtered === false) {
-    $('.filtering').slickFilter(':even');
-    $(this).text('Unfilter Slides');
-    filtered = true;
-  } else {
-    $('.filtering').slickUnfilter();
-    $(this).text('Filter Slides');
-    filtered = false;
-  }
-});
+  $('.js-filter').on('click', function(){
+    if(filtered === false) {
+      $('.filtering').slickFilter(':even');
+      $(this).text('Unfilter Slides');
+      filtered = true;
+    } else {
+      $('.filtering').slickUnfilter();
+      $(this).text('Filter Slides');
+      filtered = false;
+    }
+  });
   
   var img = $('#search_results').children().children()
   img.on('mouseenter', function(){
@@ -38,8 +47,7 @@ $('.js-filter').on('click', function(){
      all_links = $('.links');
      // all_links.slideUp();
      link.slideToggle();
-      
-      
+          
   });
 
   img.on('mouseleave', function(){
@@ -48,17 +56,9 @@ $('.js-filter').on('click', function(){
      all_links = $('.links');
       // all_links.slideDown(); 
       link.slideUp();
-
   });
     
 
-				
-});
-
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).on('ready page:load', function(){
 	$("#invitation-div").on("click",".invitation-button", function(){
 		var invitorId = window.current_user.id;
 		var invitorName = window.current_user.username;
@@ -143,7 +143,20 @@ $(document).on('ready page:load', function(){
 		});
 	});
 
- 	$("#search-div").on("click", "#search-button",function(){
+$("input[name=gender]").change(function(){
+    $(this).closest('div').find('.summary').text($(this).siblings('label').text());
+});
+
+$("input[name=age]").change(function(){
+    $(this).closest('div').find('.summary').text($(this).siblings('label').text());
+});
+
+$("input[name=city]").change(function(){
+    $(this).closest('div').find('.summary').text($(this).siblings('label').text());
+});
+
+
+ 	$(".search-tools").on("click", "#search-button",function(){
  		var genderToSearch = $('input[name=gender]:checked').val();
  		console.log(genderToSearch)
  		var ageToSearch = $('input[name=age]:checked').val();
@@ -271,8 +284,6 @@ $(document).on('ready page:load', function(){
 $(function() {
   Filter.enhance();
 });
-
-
 
 });
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,78 +6,79 @@ class UsersController < ApplicationController
     if params[:search]
       age_range = parse_age_range(params[:age])
       @users = User.by_gender(params[:gender]).by_age_range(age_range[0],age_range[1]).by_city(params[:city]).page(params[:page])
-      @user10 = @users.limit(10)
+      puts User.by_gender(params[:gender]).by_age_range(age_range[0],age_range[1]).by_city(params[:city]).length
     else
-      @users = User.order('users.created_at DESC').page(params[:page])
-      @user10 = @users.limit(10)
+      @users = User.order('users.created_at DESC').page(params[:page]) 
+      puts User.all.length
     end
-    puts @users.length
-    respond_to do |format|
-    format.html
-    format.js
-   end       
 
-  end
+      @user10 = @users.limit(10)
+      respond_to do |format|
+        format.html
+        format.js
+      end       
+    end
 
   def edit
-  	@user = User.find(params[:id])
-  end
+   @user = User.find(params[:id])
+ end
 
-  def update
-    @user = User.find(params[:id])
-    if @user.update_attributes(user_params)
-      redirect_to user_path(@user)
-    else
-      render :edit
-    end
+ def update
+  @user = User.find(params[:id])
+  if @user.update_attributes(user_params)
+    redirect_to user_path(@user)
+  else
+    render :edit
   end
+end
 
-  def show
-    @user = User.find(params[:id])
-    @favorite = Favorite.where("favoriter_id = ? AND favorited_id = ?",current_user.id, @user.id).first
-    @block = Block.where("blocker_id = ? AND blocked_id =?", current_user.id, @user.id).first
-    if current_user.id == @user.id
-      @favorite_users = current_user.favorite_users
-      p @favorite_users
-      @blocked_users = current_user.blocked_users
-      p @blocked_users
-    else
-    end
+def show
+  @user = User.find(params[:id])
+  @favorite = Favorite.where("favoriter_id = ? AND favorited_id = ?",current_user.id, @user.id).first
+  @block = Block.where("blocker_id = ? AND blocked_id =?", current_user.id, @user.id).first
+  if current_user.id == @user.id
+    @favorite_users = current_user.favorite_users
+    p @favorite_users
+    @blocked_users = current_user.blocked_users
+    p @blocked_users
+  else
   end
+end
 
-  def new
-    @user = User.new
+def new
+  @user = User.new
+end
+
+def create
+  @user = User.new(user_params)
+
+  if @user.save
+    auto_login(@user)
+    redirect_back_or_to(:users, notice: 'login successful')
+  else
+    render :new
   end
+end
 
-  def create
-    @user = User.new(user_params)
+private
+def user_params
+  params.require(:user).permit(:email, :password, :password_confirmation, :biography, :first_name, :last_name, :username, :gender, :gender_preference, :address, :date_of_birth)
+end
 
-    if @user.save
-      redirect_back_or_to(:users, notice: 'User was successfully created')
-    else
-      render :new
-    end
+def parse_age_range(age_range_str)
+  case age_range_str
+  when "18-30"
+    return [18,30]
+  when "31-40"
+    return [31,40]
+  when "41-50"
+    return [41,50]
+  when "51-60"
+    return [51,60]
+  when "61-100"
+    return [61,100]
+  else
+    return [nil, nil]
   end
-
-  private
-  def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation, :biography, :first_name, :last_name, :username, :gender, :gender_preference, :address, :date_of_birth)
-  end
-
-  def parse_age_range(age_range_str)
-      case age_range_str
-        when "18-30"
-          return [18,30]
-        when "31-40"
-          return [31,40]
-        when "41-50"
-          return [41,50]
-        when "51-60"
-          return [51,60]
-        when "61-100"
-          return [61,100]
-        else
-          return [nil, nil]
-      end
-  end
+end
 end
