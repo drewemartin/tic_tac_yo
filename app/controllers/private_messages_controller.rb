88daf14598ef46b6afd8ad13373152f7c8b34aec
zhengweiyi <zhengweiyi@gmail.com>
2014-08-05 14:52:43 -0400

    record the source page when user navigate to private_message/new, so it could navigate back once new private_message is submitted

--- app/controllers/private_messages_controller.rb
+++ app/controllers/private_messages_controller.rb
@@ -2,6 +2,10 @@ class PrivateMessagesController < ApplicationController
   
   def new
   	@user = User.find(params[:user_id])
+    @source = params[:source]
+    if @user.id == current_user.id
+      @favorite_users = @user.favorite_users
+    end
   	@private_message = @user.private_messages.build
   end
 
@@ -9,19 +13,24 @@ class PrivateMessagesController < ApplicationController
   	@user = User.find(params[:user_id])
   	@private_message = PrivateMessage.new(private_message_params)
   	@private_message.sender = current_user
-  	@private_message.recipient = @user
+  	@recipient = @private_message.recipient
+    @source = params[:source]
   	if @private_message.save
-  		str_notice = "private message sent to " +  @user.first_name + " " + @user.last_name
-  		redirect_to user_path(@user), notice: str_notice
+  		str_notice = "private message sent to " +  @recipient.first_name + " " + @recipient.last_name
+      if @user.id == current_user.id
+        redirect_to @source, notice: str_notice
+      else
+        redirect_to @source, notice: str_notice
+      end
   	else
-  		flash.now[:alert] = "failed to send private message to " +  @user.first_name + " " + @user.last_name
+  		flash.now[:alert] = "failed to send private message to " +  @recipient.first_name + " " + @recipient.last_name
   		render :new
   	end
   end
 
   def index
   	@user = User.find(params[:user_id])
-    @received_messages = @user.obtained_messages.excluding_by_senders(@user.blocked_users)
+    @received_messages = @user.obtained_messages.excluding_by_senders(@user.blocked_users).page(params[:page])
   end
 
   def show
@@ -31,15 +40,16 @@ class PrivateMessagesController < ApplicationController
 
   def destroy
   	private_message_to_destroy = PrivateMessage.find(params[:id])
-  	p private_message_to_destroy
+    recipient = private_message_to_destroy.recipient
   	private_message_to_destroy.destroy
   	respond_to do |format|
   			format.json{render json:{action_status: "success"}}
+        format.html{ redirect_to user_private_messages_path(recipient) }
   	end
   end
 
   private
   def private_message_params
-  	params.require(:private_message).permit(:message)
+  	params.require(:private_message).permit(:message, :recipient_id)
   end
 end
