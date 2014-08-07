b91ae12e9ff04b2999976197a6f6402e1b00da90
Sandy Ludosky <sandyludosky@gmail.com>
2014-08-07 12:45:55 -0400

    updated winnig banner on games#show

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -188,7 +188,7 @@ $(document).on('ready page:load', function(){
     function updateWin (win) {
       if (win !==  "unknown")
       {
-        $("#game-result").html("<div><span>"+ tie + " click here to play again" +"</span></div>");
+        $("#game-result").html("<a href='#'>"+ win + "'s click here to play again" +"</a>");
         $('#game-result').show();
         $('#reset').off('click');
         $('td').off('click');
@@ -204,7 +204,7 @@ $(document).on('ready page:load', function(){
     function updateTie (tie) {
       if (tie !==  "no_tie")
       {
-        $("#game-result").html("<div><span>"+ tie + " click here to play again" +"</span></div>");
+        $("#game-result").html("<a href='#'>"+ win + "'s click here to play again" +"</a>");
         $('#game-result').show();
         $('#reset').off('click');
         $('td').off('click');
